9446093436a47ed95deca79501e1bfd290d529eb
kskochneva <0758299@gmail.com>
2026-05-19 14:46:22 +0300

    Update server.hpp

--- server.hpp
+++ server.hpp
@@ -7,6 +7,9 @@
 #include <string>
 #include <vector>
 
+#include <vector>
+#include <mutex>
+
 namespace asio = boost::asio;
 using tcp = asio::ip::tcp;
 using error_code = boost::system::error_code;
@@ -16,6 +19,8 @@ class Session : public std::enable_shared_from_this<Session> {
 public:
     Session(tcp::socket socket, asio::io_context& io_context);
     void start();  // начало обслуживания клиента
+    void log_message(const std::string& msg);
+//для задачи 4
 
 private:
     void do_read();                    // чтение запроса от клиента
@@ -27,6 +32,7 @@ private:
     asio::io_context& io_context_;
     asio::streambuf read_buffer_;      // буфер для чтения
     asio::steady_timer timer_;         // таймер для задачи 3
+    asio::strand<asio::io_context::executor_type> strand_;//для последоватльности операций
 };
 
 // Класс сервера (принимает подключения)
@@ -34,10 +40,15 @@ class Server {
 public:
     Server(asio::io_context& io_context, unsigned short port);
     void start();
-
+    void add_to_log(const std::string& entry);
+    std::vector<std::string> get_log() const;
 private:
-    void do_accept();  // асинхронное принятие подключения
+    void do_accept();  // асинхронное принятие подключения 
 
     asio::io_context& io_context_;
+    std::vector<std::string> log_;
+    mutable std::mutex log_mutex_;
+     // НОВОЕ: strand для потокаобезопасного доступа к логу
+    asio::strand<asio::io_context::executor_type> log_strand_;
     tcp::acceptor acceptor_;
 };
